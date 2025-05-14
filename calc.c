9ad84235163027e9656d801c832fa6b4ad8a2bae
Anshikapundir1 <161428274+Anshikapundir1@users.noreply.github.com>
2025-05-14 07:35:09 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,7 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main() {
     int a;
-    a = 2;
-    printf("Value of a");
+    int b;
+    
+    
+    printf("Enter value of a");
+    scanf("%d", &a);
+    printf("Enter value of b");
+    scanf("%d", &b);
+    
+
     return 0;
 }
+
