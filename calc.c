d263757adc7624f30cba0ac1769989f72cf97824
Anshikapundir1 <161428274+Anshikapundir1@users.noreply.github.com>
2025-05-14 07:56:18 +0530

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    int a;
-    int b;
-    
-    
-    printf("Enter value of a");
-    scanf("%d", &a);
-    printf("Enter value of b");
-    scanf("%d", &b);
-    
+    int num;
+    int i;
+    i=0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("You entered a positive number\n");
+    } else if (num < 0) {
+        printf("You entered a negative number\n");
+    } else {
+        printf("You entered zero\n");
+    }
+
+    printf("Counting from 1 to %d\n", num);
+
+    while (i < num) {
+        i++;
+        printf("%d\n", i);
+    }
 
     return 0;
 }
-
